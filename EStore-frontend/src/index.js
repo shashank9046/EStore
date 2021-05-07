487d21044cb5703292934b312315549a55f2205f
Shashank  Pandey <skppandey2017@gmail.com>
2021-05-07 21:29:28 +0530

    initial setup complete

--- EStore-frontend/src/index.js
+++ EStore-frontend/src/index.js
@@ -1,5 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Routes from "./Routes";
-
-ReactDOM.render(<Routes />, document.getElementById("root"));
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Routes from './Routes'
+ReactDOM.render(<Routes />, document.getElementById('root'))
