cc8a79ef94e92498dc8e47e77ed27c58f4c230b0
Shashank  Pandey <skppandey2017@gmail.com>
2021-05-10 20:15:16 +0530

    updates

--- config/production.js
+++
@@ -1,7 +0,0 @@
-MONGODB_URI =
-  'mongodb+srv://skp9046:Hellogfund@123@estore.zgexp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-PORT = 5000
-JWT_SECRET = querty
-BRAINTREE_MERCHANT_ID = '4kcx3jpqmqr9j42s'
-BRAINTREE_PUBLIC_KEY = 't5f2h6gjzpmvs2pq'
-BRAINTREE_PRIVATE_KEY = '2e9d7d2a6963bea4ac32cd6ea3111bdd'
